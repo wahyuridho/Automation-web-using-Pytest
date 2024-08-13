9780683f44a86674b24b73a7ef0913a13b10665e
wahyuridho <saputroridho569@gmail.com>
2024-08-13 14:13:07 +0700

    menyederhanakan code di confest.py pada function get_browser

--- src/conftest.py
+++ src/conftest.py
@@ -1,10 +1,10 @@
-import pytest
 import os
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope="module")    
+@pytest.fixture(scope="module")
 def browser():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.implicitly_wait(15)
@@ -13,21 +13,17 @@ def browser():
 
 def get_browser_version():
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # Menjalankan browser dalam mode headless
-    driver = webdriver.Chrome(options=options)
-    browser_version = driver.capabilities['browserVersion']
-    driver.quit()
-    return browser_version
+    options.add_argument('--headless')
+    with webdriver.Chrome(options=options) as driver:
+        return driver.capabilities['browserVersion']
 
 @pytest.fixture(scope='session', autouse=True)
 def create_environment_properties():
     browser_version = get_browser_version()
     allure_results_dir = 'allure-results'
-    if not os.path.exists(allure_results_dir):
-        os.makedirs(allure_results_dir)
+    os.makedirs(allure_results_dir, exist_ok=True)
 
-    environment_file = os.path.join(allure_results_dir, 'environment.properties')
-    with open(environment_file, 'w') as file:
+    with open(os.path.join(allure_results_dir, 'environment.properties'), 'w') as file:
         file.write(f'Browser=Chrome\n')
         file.write(f'Browser.Version={browser_version}\n')
         file.write('OS=Windows 11\n')
